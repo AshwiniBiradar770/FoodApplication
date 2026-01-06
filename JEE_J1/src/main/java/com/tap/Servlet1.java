285b47ed8d7d92cdb04bbc06b75176d1aff3d0ec
AshwiniBiradar770 <ashwinibiradar770@gmail.com>
2026-01-06 20:30:03 +0530

    This is my firstcommit

---
+++ JEE_J1/src/main/java/com/tap/Servlet1.java
@@ -0,0 +1,24 @@
+package com.tap;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Servlet1 extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		
+		String name = req.getParameter("name");
+		
+		PrintWriter out = resp.getWriter();	
+		
+		out.println("Hi, " +name+ " Welcome to HomePage");
+		
+	}
+	
+}
